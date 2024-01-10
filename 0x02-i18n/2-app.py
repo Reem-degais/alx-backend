02476234cecb37d9a8445994493f7bcc064306b4
Reem-degais <reem.degais.33@gmail.com>
2024-01-10 01:08:31 +0000

    -

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -16,6 +16,7 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -26,7 +27,8 @@ babel = Babel(app)
 @babel.localeselector
 def get_locale():
     """determines the best match with our supported languages."""
-     return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index():
