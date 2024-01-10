93952b4463eb4a9cd76440f978cd7e22e01c2f59
Reem-degais <reem.degais.33@gmail.com>
2024-01-10 02:03:34 +0000

    -

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -28,7 +28,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', , strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
     """default route"""
     return render_template("3-index.html",)
