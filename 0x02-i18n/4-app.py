7c5bb233aa8520624c176fd367d23488d11aa916
Reem-degais <reem.degais.33@gmail.com>
2024-01-10 02:25:07 +0000

    -

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+Use the _ or gettext function to parametrize your templates
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """determines the best match with our supported languages."""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """default route"""
+    return render_template("3-index.html",)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
