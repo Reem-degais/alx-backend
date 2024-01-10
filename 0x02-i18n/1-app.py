a74629328f1a2d172f9d7f008baff3d47cecdab6
Reem-degais <reem.degais.33@gmail.com>
2024-01-10 00:30:50 +0000

    -

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 """
-Then instantiate the Babel object in your app. 
+Then instantiate the Babel object in your app.
 Store it in a module-level variable named babel.
-
 In order to configure available languages in our app,
 you will create a Config class that has a LANGUAGES class
 attribute equal to ["en", "fr"].
-
 Use Config to set Babel’s default locale ("en") and timezone ("UTC").
-
 Use that class as config for your Flask app.
 """
 
